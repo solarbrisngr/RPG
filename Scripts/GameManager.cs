c85dd3583186fde7ed47e3698b73349067803414
solarbrisngr <59808313+solarbrisngr@users.noreply.github.com>
2020-10-10 18:45:45 -0700

    Add files via upload

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System.Linq;
+
+public class GameManager : MonoBehaviourPun
+{
+    [Header("Players")]
+    public string playerPrefabPath;
+    public Transform[] spawnPoints;
+    public float respawnTime;
+    private int playersInGame;
+    // instance
+    public static GameManager instance;
+    public PlayerController[] players;
+
+    void Awake()
+    {
+        instance = this;
+    }
+    
+    [PunRPC]
+    void ImInGame()
+    {
+        playersInGame++;
+        if (playersInGame == PhotonNetwork.PlayerList.Length)
+            SpawnPlayer();
+    }
+    void Start()
+    {
+        photonView.RPC("ImInGame", RpcTarget.AllBuffered);
+        players = new PlayerController[PhotonNetwork.PlayerList.Length];
+    }
+    void SpawnPlayer()
+    {
+        GameObject playerObj = PhotonNetwork.Instantiate(playerPrefabPath, spawnPoints[Random.Range(0, spawnPoints.Length)].position, Quaternion.identity);
+        // initialize the player
+        playerObj.GetComponent<PhotonView>().RPC("Initialize", RpcTarget.All, PhotonNetwork.LocalPlayer);
+
+    }
+}
